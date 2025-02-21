69e3c12cc79f1e35019529fe561e8084647a1ae9
krwebb90 <krwebb90@gmail.com>
2025-02-21 13:53:42 -0600

    updating old lessons in new format

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/Band Name Generator Project/main.py
@@ -0,0 +1,6 @@
+
+print("Hello, welcome to the band name generator!")
+city = input("Where were you born? ")
+pet = input("What is the name of your favorite pet? ")
+
+print(f"Your band name is {city} + {pet}!")
---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,15 @@
+# imports
+from art import *
+
+# art
+print(logo)
+
+# define game
+# global score variable?
+
+
+
+# conditions (win or lose)
+
+
+# add points or game over
