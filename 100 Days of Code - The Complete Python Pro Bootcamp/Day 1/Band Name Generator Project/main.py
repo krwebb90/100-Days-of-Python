d9b8551433fc6a0774a9e1a58b3a5f204ed3304f
krwebb90 <krwebb90@gmail.com>
2025-02-21 14:00:10 -0600

    more old updates

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/Band Name Generator Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/Band Name Generator Project/main.py
@@ -3,4 +3,4 @@ print("Hello, welcome to the band name generator!")
 city = input("Where were you born? ")
 pet = input("What is the name of your favorite pet? ")
 
-print(f"Your band name is {city} + {pet}!")
+print(f"Your band name is {city} {pet}!")
