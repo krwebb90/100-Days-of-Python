d6122030c31427710ea57a715b18494e91428f42
krwebb90 <krwebb90@gmail.com>
2025-02-21 14:52:08 -0600

    more updates for prior lessons

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Multiple Ifs/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 3/Multiple Ifs/task.py
@@ -1,5 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster")
@@ -10,5 +11,11 @@ if height >= 120:
         print("Please pay $7.")
     else:
         print("Please pay $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
