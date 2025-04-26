c6de5edcc872e4153c7c777f23b8516b5b1f35e2
krwebb90 <158116197+krwebb90@users.noreply.github.com>
2025-04-26 13:32:14 -0500

    finishing higher/lower game with additional functionality

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 14/Higher or Lower Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 14/Higher or Lower Project/main.py
@@ -2,35 +2,71 @@
 from art import *
 import random
 import game_data
+import os
+
 
 # art
 print(logo)
 
-# define game
+# check for vowel to clean up verbiage
+def vowel_check(word):
+    return 'an' if word[0].lower() in 'aeiou' else 'a'
+
+# takes the account data and returns the printable format
+def format_data(account):
+    account_name = account['name']
+    account_desc = account['description']
+    account_country = account['country']
+    return f"{account_name}, {vowel_check(account_desc)} {account_desc}, from {account_country}"
+
+# takes the user's guess and checks if they chose the higher number of the two random accounts. both return statements act as boolean checks 
+def check_answer(user_guess, accountA_followers, accountB_followers):
+    if accountA_followers > accountB_followers:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
+    
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    # for mac and linux
+    else:
+        _ = os.system('clear')
 
 # global score variable?
+user_score = 0
 
-# define a vs b, then increment the score or game over, maybe a while true loop
-
-# random choice a, remove a from the list, random choice b
-# this has to be in the loop so when the game restarts, we don't lose the original values
-
-entirechoiceA = random.choice(game_data.data)
-choiceA = entirechoiceA['name']
-remaininglist = game_data.data.copy()
-remaininglist.remove(entirechoiceA)
-entirechoiceB = random.choice(remaininglist)
-choiceB = entirechoiceB['name']
-
-userchoice = input(f'Who has more instagram followers? {choiceA} or {choiceB}? ')
+# flag for loop
+game_should_continue = True
 
 
+# define game in loop
+while game_should_continue:
 
+    # random choice a, remove a from the list, random choice b
+    # this has to be in the loop so when the game restarts, we don't lose the original values
+    # determine best place to start and stop loop, this is the right direction, need to package in loops and defined functions
+    entireaccountA = random.choice(game_data.data)
+    accountA = entireaccountA['name']
+    remaininglist = game_data.data.copy()
+    remaininglist.remove(entireaccountA)
+    entireaccountB = random.choice(remaininglist)
+    accountB = entireaccountB['name']
 
-# conditions (win or lose)
+    userchoice = input(f'Who has more instagram followers? \n\n "A": {format_data(entireaccountA)} \n {vs} \n "B": {format_data(entireaccountB)}? \n\n Type A or B: ').lower()
 
+    clear()
+    print(logo)
 
-# add points or game over
+    # check if the user's guess is correct
+    # if statement for the follower count of entirechoiceA vs entirechoiceB
+    is_correct = check_answer(userchoice, entireaccountA['follower_count'], entireaccountB['follower_count'])
 
-
-# code below
+    # conditions (win or lose)
+    # increment score (the actual solution wants to move the choiceB to choiceA if it is higher so we are always comparing to choiceA but I don't like that) or game over
+    if is_correct:
+        user_score += 1
+        print(f"You are correct! Your current score is {user_score}")
+    else:
+        print(f"Sorry, that is incorrect. Game Over!\n\n Your final score is {user_score}")
+        game_should_continue = False
