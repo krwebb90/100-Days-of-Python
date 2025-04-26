456fe7954d60d45344c6aa170e676b063726dce8
krwebb90 <158116197+krwebb90@users.noreply.github.com>
2025-04-26 10:56:33 -0500

    First portion of Day14 Higher/Lower game

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 14/Higher or Lower Project/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 14/Higher or Lower Project/main.py
@@ -1,11 +1,29 @@
 # imports
 from art import *
+import random
+import game_data
 
 # art
 print(logo)
 
 # define game
+
 # global score variable?
+
+# define a vs b, then increment the score or game over, maybe a while true loop
+
+# random choice a, remove a from the list, random choice b
+# this has to be in the loop so when the game restarts, we don't lose the original values
+
+entirechoiceA = random.choice(game_data.data)
+choiceA = entirechoiceA['name']
+remaininglist = game_data.data.copy()
+remaininglist.remove(entirechoiceA)
+entirechoiceB = random.choice(remaininglist)
+choiceB = entirechoiceB['name']
+
+userchoice = input(f'Who has more instagram followers? {choiceA} or {choiceB}? ')
+
 
 
 
@@ -13,3 +31,6 @@ print(logo)
 
 
 # add points or game over
+
+
+# code below
