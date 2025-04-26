4d2366d228b6772c86f683da47e7b1edfb36cde8
krwebb90 <158116197+krwebb90@users.noreply.github.com>
2025-04-25 22:18:01 -0500

    testing

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/main_lesson.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/main_lesson.py
@@ -1,20 +1,20 @@
 import turtle
 
-timmy = turtle.Turtle()
+# timmy = turtle.Turtle()
 
-timmy.shape("turtle")
-timmy.color("DarkSeaGreen2")
-
-
-my_screen = turtle._Screen()
-print(my_screen)
-print(timmy)
-
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+# timmy.shape("turtle")
+# timmy.color("DarkSeaGreen2")
 
 
+# my_screen = turtle._Screen()
+# print(my_screen)
+# print(timmy)
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
 
 
-turtle.mainloop()
+
+
+# turtle.mainloop()
