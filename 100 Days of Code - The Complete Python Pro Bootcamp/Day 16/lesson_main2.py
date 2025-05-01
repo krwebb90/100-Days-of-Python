c4127899b2df4450eb6ec2656d31772947cbf73f
krwebb90 <158116197+krwebb90@users.noreply.github.com>
2025-05-01 14:19:37 -0500

    renaming and updating Day 16

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/lesson_main2.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/lesson_main2.py
@@ -4,9 +4,12 @@ from prettytable import PrettyTable
 
 table = PrettyTable()
 
+
 table.add_column("Pokemon",["Pikachu","Squirtle","Charmander"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
 
+table.header = True
+table.border = True
 table.align = "l"
 
 print(table)
