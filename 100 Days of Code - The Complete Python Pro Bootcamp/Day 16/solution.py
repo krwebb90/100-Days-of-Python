b3f706f75b59d02229a82f3274f78df95fb2e277
krwebb90 <158116197+krwebb90@users.noreply.github.com>
2025-05-02 12:34:10 -0500

    OOP of coffeemaker project

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/solution.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 16/solution.py
@@ -2,18 +2,15 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
-
-
 
 is_on = True
 
 while is_on:
     options = menu.get_items()
-    choice = input(f"What drink would you like? ({options}):")
+    choice = input(f"What would you like? ({options}): ")
     if choice == "off":
         is_on = False
     elif choice == "report":
@@ -21,5 +18,6 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
+        
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+          coffee_maker.make_coffee(drink)
