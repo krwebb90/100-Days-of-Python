13a8bbe3c7702ed4f98686734913bf862ca63d0d
krwebb90 <158116197+krwebb90@users.noreply.github.com>
2025-05-22 17:05:39 -0500

    added dashes to multiple shapes

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 18/odd_shapes.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 18/odd_shapes.py
@@ -10,11 +10,25 @@ screen = turtle.Screen()
 
 color = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
+# lines
 def draw_shape(num_sides):
     angle = 360/num_sides
     for _ in range(num_sides):
         tim.forward(100)
         tim.right(angle)
+
+# dashes
+# def draw_shape(num_sides):
+#     angle = 360/num_sides
+#     for _ in range(num_sides):
+#         count = 0
+#         while count < 10:
+#             tim.forward(10)
+#             tim.penup()
+#             tim.forward(10)
+#             tim.pendown()
+#             count += 1
+#         tim.right(angle)
 
 for shape_side_n in range(3, 11):
     tim.color(random.choice(color))
